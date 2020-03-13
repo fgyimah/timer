cc48529c70b582836b299df4cbfbe5ccddfccbcd
Francis Gyimah <fgyimah883@gmail.com>
2020-03-13 20:54:46 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,114 @@
+let timerObj = {
+  minutes: 0,
+  seconds: 0,
+  timerId: 0
+};
+
+function soundAlarm() {
+  let amount = 3;
+  let audio = new Audio('timer_effect.mp3');
+
+  function playSound() {
+    audio.pause();
+    audio.currentTime = 9;
+    audio.play();
+  }
+
+  for (let i = 0; i < amount; i++) {
+    setTimeout(playSound, 1200 * i);
+  }
+}
+
+function updateValue(key, value) {
+  if (value < 0) {
+    value = 0;
+    console.log('Positive numbers only');
+  }
+
+  if (key == 'seconds') {
+    if (value < 10) {
+      value = '0' + value;
+    }
+
+    if (value > 59) {
+      value = 59;
+    }
+  }
+
+  $('#' + key).html(value || 0);
+  timerObj[key] = value;
+}
+
+(function detectChanges(key) {
+  console.log('Detect changes');
+
+  let input = '#' + key + '-input';
+
+  $(input).change(function() {
+    updateValue(key, $(input).val());
+  });
+
+  $(input).keyup(function() {
+    updateValue(key, $(input).val());
+  });
+
+  return arguments.callee;
+})('minutes')('seconds');
+
+function startTimer() {
+  buttonManager(['start', false], ['pause', true], ['stop', true]);
+  freezeInput();
+
+  timerObj.timerId = setInterval(function() {
+    timerObj.seconds--;
+    if (timerObj.seconds < 0) {
+      if (timerObj.minutes == 0) {
+        soundAlarm();
+        return stopTimer();
+      }
+      timerObj.seconds = 59;
+      timerObj.minutes--;
+    }
+
+    updateValue('minutes', timerObj.minutes);
+    updateValue('seconds', timerObj.seconds);
+  }, 1000);
+}
+function stopTimer() {
+  clearInterval(timerObj.timerId);
+  buttonManager(['start', true], ['pause', false], ['stop', false]);
+  unFreezeInput();
+
+  updateValue('minutes', $('#minutes-input').val());
+  let seconds = $('#seconds-input').val();
+
+  if (seconds < 10) {
+    seconds = '0' + seconds;
+  }
+  updateValue('seconds', seconds);
+}
+function pauseTimer() {
+  buttonManager(['start', true], ['pause', false], ['stop', true]);
+  clearInterval(timerObj.timerId);
+}
+
+function buttonManager(...buttonsArray) {
+  for (let i = 0; i < buttonsArray.length; i++) {
+    let button = '#' + buttonsArray[i][0] + '-button';
+    if (buttonsArray[i][1]) {
+      $(button).removeAttr('disabled');
+    } else {
+      $(button).attr('disabled', 'disabled');
+    }
+  }
+}
+
+function freezeInput() {
+  $('#minutes-input').attr('disabled', 'disabled');
+  $('#seconds-input').attr('disabled', 'disabled');
+}
+
+function unFreezeInput() {
+  $('#minutes-input').removeAttr('disabled');
+  $('#seconds-input').removeAttr('disabled');
+}
